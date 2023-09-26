932aa8c573c9c8a1ed1745caf54e18b1b686c7c6
Soowan Lee <lsy9406@gmail.com>
2023-09-26 21:37:40 +0900

    add 알파벳의 위치 찾기

--- src/main/java/com/example/algorithm/baekjoon/string/P10809.java
+++ src/main/java/com/example/algorithm/baekjoon/string/P10809.java
@@ -1,9 +1,11 @@
 package com.example.algorithm.baekjoon.string;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class P10809 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         /**
          * 알파벳 찾기
          * 1. 알파벳 소문자로만 이루어진 단어 S가 주어진다. 단어의 길이는 100을 넘지 않는다.
@@ -11,33 +13,30 @@ public class P10809 {
          * 3. 포함되어 있지 않은 경우에는 -1을 출력하시오
          */
 
-        //1. 배열 사용
-        Scanner sc = new Scanner(System.in);
-
+        //array + charAt()
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int[] arr = new int[26];
 
-        //arr[]의 모든 인덱스를 -1로 초기화 한다.
         for (int i = 0; i < arr.length; i++) {
             arr[i] = -1;
         }
 
-        String s = sc.nextLine(); //sc로 입력받은 문자열을 한죽씩 읽어온다.
-
+        String s = br.readLine();
         for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
+            char c = s.charAt(i);
 
-            if (arr[ch - 'a'] == -1) {
-                arr[ch - 'a'] = i;
+            if (arr[c - 'a'] == -1) {
+                arr[c - 'a'] = i;
             }
         }
-        for (int var : arr) {
-            System.out.print(var + " ");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
 
-        //2. indexOf() 사용
-        String str = sc.next();
+        //indexOf()
         for (char c = 'a'; c <= 'z'; c++) {
-            System.out.print(str.indexOf(c) + " ");
+            System.out.print(s.indexOf(c) + " ");
         }
     }
 }
