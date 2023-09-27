70d65ee9d5365227a66ee500a55b2c76194c1fba
Soowan Lee <lsy9406@gmail.com>
2023-09-27 12:35:52 +0900

    add 단어의 개수(공백을 기준으로 단어를 세다)

---
+++ src/main/java/com/example/algorithm/baekjoon/string/P1152.java
@@ -0,0 +1,38 @@
+package com.example.algorithm.baekjoon.string;
+
+import java.io.IOException;
+
+public class P1152 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 단어의 개수
+         * 공백을 기준으로 단어를 세어라
+         */
+
+        //BufferedReader() + StringTokenizer()
+        /*BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        System.out.println(st.countTokens());*/
+
+        int count = 0;
+        int pre_str = 32;
+        int str;
+
+        while (true) {
+            str = System.in.read();
+            if (str == 32) {
+                if (pre_str != 32) {
+                    count++;
+                }
+            } else if (str == 10) {
+                if (pre_str != 32) {
+                    count++;
+                }
+                break;
+            }
+            pre_str = str;
+        }
+        System.out.println(count);
+    }
+}
