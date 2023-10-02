608ed76f17802f230efde55abba7c87a396f5851
Soowan Lee <lsy9406@gmail.com>
2023-10-02 23:05:56 +0900

    add 원래 배열과 입력 받은 배열의 차 구하기

--- src/main/java/com/example/algorithm/baekjoon/advanced1/P3003.java
+++ src/main/java/com/example/algorithm/baekjoon/advanced1/P3003.java
@@ -1,2 +1,30 @@
-package com.example.algorithm.baekjoon.advanced1;public class P3003 {
+package com.example.algorithm.baekjoon.advanced1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P3003 {
+    public static void main(String[] args) throws IOException {
+        /**
+         * 킹, 퀸, 룩, 비숍, 나이트, 폰
+         */
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] requiredPieces = {1, 1, 2, 2, 2, 8};
+        int[] playerPieces = new int[6];
+
+        for (int i = 0; i < playerPieces.length; i++) {
+            playerPieces[i] = Integer.parseInt(st.nextToken());
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < 6; i++) {
+            int difference = requiredPieces[i] - playerPieces[i];
+            sb.append(difference).append(" ");
+        }
+        System.out.println(sb);
+    }
 }
