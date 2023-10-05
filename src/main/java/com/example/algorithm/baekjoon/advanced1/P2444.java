8186cb353ba1b5f550d240b8d6b2f349b22aca2b
Soowan Lee <lsy9406@gmail.com>
2023-10-05 14:47:55 +0900

    별찍기 복습

--- src/main/java/com/example/algorithm/baekjoon/advanced1/P2444.java
+++ src/main/java/com/example/algorithm/baekjoon/advanced1/P2444.java
@@ -37,12 +37,10 @@ public class P2444 {
             for (int j = 1; j <= n - i; j++) {
                 System.out.print(" ");
             }
-
             for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-
     }
 }
