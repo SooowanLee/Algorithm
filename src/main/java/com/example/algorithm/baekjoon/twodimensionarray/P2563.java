64d230cebfcaac8aa56474eda4010e284a29b0fc
Soowan Lee <lsy9406@gmail.com>
2023-10-19 23:58:21 +0900

    add 2차원 배열에서 겹치는 면적 제외하고 면적구하기

--- src/main/java/com/example/algorithm/baekjoon/twodimensionarray/P2563.java
+++ src/main/java/com/example/algorithm/baekjoon/twodimensionarray/P2563.java
@@ -9,23 +9,22 @@ public class P2563 {
     public static void main(String[] args) throws IOException {
         /**
          * 색종이
-         *
          */
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int total = 0; //검은 영역의 넓이
+        int total = 0; //겹치는 면적을 뺀 총면적
         int n = Integer.parseInt(br.readLine()); //색종이 갯수
-        boolean[][] arr = new boolean[101][101]; //도화지
+        boolean[][] arr = new boolean[101][101]; //도화지 크기
 
         for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            //(x,y)부터 (x+9,y+9)까지의 영역을 하나씩 체크한 후 총 넓이에 더해준다.
-            for (int j = x; j < x + 10; j++) {
+            StringTokenizer st = new StringTokenizer(br.readLine()); //색종이 위치
+            int x = Integer.parseInt(st.nextToken()); //색종이 x축 시작
+            int y = Integer.parseInt(st.nextToken()); //색종이 y축 시작
+
+            for (int j = x; j < x + 10; j++) {        //색종이 그리기
                 for (int k = y; k < y + 10; k++) {
-                    if (!arr[j][k]) {
-                        arr[j][k] = true;
-                        total++;
+                    if (!arr[j][k]) {                 //겹치는 부분 확인
+                        arr[j][k] = true;             
+                        total++;                      //겹치지 않으면 총면적에 더하기
                     }
                 }
             }
