71ec92951cc757bf441921b51c6d050d18f5722b
Soowan Lee <lsy9406@gmail.com>
2024-07-31 18:58:57 +0900

    feat 알고리즘 문제 풀이

---
+++ src/main/java/com/example/algorithm/programmers/basic/P181930.java
@@ -0,0 +1,4 @@
+package com.example.algorithm.programmers.basic;
+
+public class P181930 {
+}
--- src/main/java/com/example/algorithm/programmers/basic/P181936.java
+++ src/main/java/com/example/algorithm/programmers/basic/P181936.java
@@ -2,8 +2,8 @@ package com.example.algorithm.programmers.basic;
 
 /**
  * 공배수
- * 정수 num과 n이 매개 변수로 주어질 때, num이 n의 배수이면 1을 return
- * n의 배수가 아니라면 0을 return
+ * 정수 number와 n, m이 주어집니다.
+ * number가 n의 배수이면서 m의 배수이면 1을 아니라면 0을 return
  */
 public class P181936 {
     public static void main(String[] args) {
