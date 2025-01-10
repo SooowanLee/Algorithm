39e42c6243c7fa4de21c670d463f51af0b656a43
Soowan Lee <lsy9406@gmail.com>
2025-01-10 20:22:35 +0900

    add 문자열 섞기, 나누어 떨어지는 숫자 배열, 음양 더하기, 없는 숫자 더하기

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P12943_콜라츠_추측.java
@@ -0,0 +1,22 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P12943_콜라츠_추측 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(6));
+        System.out.println(Solution.solution(16));
+        System.out.println(Solution.solution(626331));
+    }
+
+    class Solution {
+
+
+        public static int solution(int num) {
+            int answer = num;
+            int count = 0;
+
+
+
+            return answer;
+        }
+    }
+}
---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P86051_없는_숫자_더하기.java
@@ -0,0 +1,31 @@
+package com.example.algorithm.programmers.basic_two;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P86051_없는_숫자_더하기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new int[]{1,2,3,4,6,7,8,0}));
+        System.out.println(Solution.solution(new int[]{5,8,4,0,6,7,9}));
+    }
+
+
+    class Solution {
+        public static int solution(int[] numbers) {
+            int answer = 0;
+            List<Integer> numberList = new ArrayList<>();
+
+            for (int number : numbers) {
+                numberList.add(number);
+            }
+
+            for (int i = 0; i <=9; i++) {
+                if (!numberList.contains(i)) {
+                    answer += i;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
