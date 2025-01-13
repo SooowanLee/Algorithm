bcc322489ef1f0e850651b1ca318bef72fb0dc8a
Soowan Lee <lsy9406@gmail.com>
2025-01-13 17:20:26 +0900

    add 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P181880_1로_만들기.java
@@ -0,0 +1,14 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P181880_1로_만들기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution());
+    }
+
+    class Solution {
+        public static int solution(int[] num_list) {
+            int answer = 0;
+            return answer;
+        }
+    }
+}
