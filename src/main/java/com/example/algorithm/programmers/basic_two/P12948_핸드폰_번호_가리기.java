232c7ef40ce450eca4b5b98e05581079c65eae8c
Soowan Lee <lsy9406@gmail.com>
2025-01-13 21:49:07 +0900

    add 핸드폰 번호 가리기

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P12948_핸드폰_번호_가리기.java
@@ -0,0 +1,31 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P12948_핸드폰_번호_가리기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("01033334444"));
+        System.out.println(Solution.solution("027778888"));
+    }
+
+    class Solution {
+        public static String solution(String phone_number) {
+            //StringBuilder 사용한 방법
+            StringBuilder sb = new StringBuilder();
+            String substring = phone_number.substring(phone_number.length() - 4);
+
+            for (int i = 0; i < phone_number.length() - 4; i++) {
+                sb.append("*");
+            }
+            sb.append(substring);
+
+            return sb.toString();
+
+            //고전적인 방법
+//            char[] charArray = phone_number.toCharArray();
+//            for (int i = 0; i < charArray.length - 4; i++) {
+//                charArray[i] = '*';
+//            }
+//
+//            return String.valueOf(charArray);
+        }
+    }
+}
