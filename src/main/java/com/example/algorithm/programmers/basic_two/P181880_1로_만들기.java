a3caa9ca80edee95da1703007662e594305b2454
Soowan Lee <lsy9406@gmail.com>
2025-01-13 19:55:38 +0900

    add 1로 만들기

--- src/main/java/com/example/algorithm/programmers/basic_two/P181880_1로_만들기.java
+++ src/main/java/com/example/algorithm/programmers/basic_two/P181880_1로_만들기.java
@@ -2,12 +2,25 @@ package com.example.algorithm.programmers.basic_two;
 
 public class P181880_1로_만들기 {
     public static void main(String[] args) {
-        System.out.println(Solution.solution());
+        System.out.println(Solution.solution(new int[]{12, 4, 15, 1, 14}));
     }
 
     class Solution {
         public static int solution(int[] num_list) {
             int answer = 0;
+
+            for (int num : num_list) {
+                int j = num; //num_list의 각 원소에 대해 연산
+                while (j > 1) { //j가 1보다 클 동안 반복
+                    if (j % 2 == 0) {
+                        j /= 2;
+                    } else {
+                        j = (j - 1) / 2;
+                    }
+                    answer++; //1이 되기 전 연산 횟수만 카운트
+                }
+            }
+
             return answer;
         }
     }
