230f0e8b74c62f630d700678590257bcb4f03ab7
Soowan Lee <lsy9406@gmail.com>
2025-01-17 23:12:23 +0900

    feat 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P12903_가운데_글자_가져오기.java
@@ -0,0 +1,25 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P12903_가운데_글자_가져오기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("abcde"));
+        System.out.println(Solution.solution("qwer"));
+    }
+
+
+    //가운데를 찾기 위해 2로 나눈다.
+    class Solution {
+        public static String solution(String s) {
+            String answer = "";
+            int i = s.length() / 2;
+
+            if (s.length() % 2 == 0) {
+                answer = s.substring(i-1, i+1);
+            } else {
+                answer = s.substring(i, i+1);
+            }
+
+            return answer;
+        }
+    }
+}
---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P181917_간단한_논리_연산.java
@@ -0,0 +1,14 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P181917_간단한_논리_연산 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(false, true, true, true));
+        System.out.println(Solution.solution(true, false, false, false));
+    }
+
+    class Solution {
+        public static boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+            return (x1 || x2) && (x3 || x4);
+        }
+    }
+}
