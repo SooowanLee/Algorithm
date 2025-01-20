14bd856de4381c59f1f21a8253433314fd54b2f7
Soowan Lee <lsy9406@gmail.com>
2025-01-20 22:04:49 +0900

    feat 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P181902_문자_개수_세기.java
@@ -0,0 +1,28 @@
+package com.example.algorithm.programmers.basic_two;
+
+import java.util.Arrays;
+
+public class P181902_문자_개수_세기 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(Solution.solution("Programmers")));
+    }
+
+    //대문자가 먼저 나오기 때문에
+    //소문자는 대문자가 끝나는 지점인 26다음 27번째라서
+    //26을 더해준다.
+    class Solution {
+        public static int[] solution(String my_string) {
+            int[] answer = new int[52];
+
+            for (char c : my_string.toCharArray()) {
+                if (c >= 'A' && c <= 'Z') { //대문자인 경우
+                    answer[c - 'A']++;
+                } else {
+                    answer[c - 'a' + 26]++; //소문자인 경우
+                }
+            }
+
+            return answer;
+        }
+    }
+}
