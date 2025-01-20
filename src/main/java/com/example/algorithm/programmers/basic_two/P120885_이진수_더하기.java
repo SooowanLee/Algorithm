6ba06d1e5aa8524d6e3f71d56bd66c0b65f401bd
Soowan Lee <lsy9406@gmail.com>
2025-01-20 19:02:24 +0900

    feat 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P120885_이진수_더하기.java
@@ -0,0 +1,18 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P120885_이진수_더하기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("10", "11"));
+        System.out.println(Solution.solution("1001", "1111"));
+    }
+
+    class Solution {
+        public static String solution(String bin1, String bin2) {
+            String answer = "";
+
+
+
+            return answer;
+        }
+    }
+}
