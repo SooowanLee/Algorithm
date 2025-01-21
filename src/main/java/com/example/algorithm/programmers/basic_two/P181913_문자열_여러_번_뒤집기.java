7b778a81100a035b8de7c41965dff262648aa88e
Soowan Lee <lsy9406@gmail.com>
2025-01-21 15:46:46 +0900

    feat while()을 사용하여 문자열 뒤집기

---
+++ src/main/java/com/example/algorithm/programmers/basic_two/P181913_문자열_여러_번_뒤집기.java
@@ -0,0 +1,26 @@
+package com.example.algorithm.programmers.basic_two;
+
+public class P181913_문자열_여러_번_뒤집기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("rermgorpsam", new int[][]{{2, 3}, {0, 7}, {5, 9}, {6, 10}}));
+    }
+
+    class Solution {
+        public static String solution(String my_string, int[][] queries) {
+            char[] charArray = my_string.toCharArray();
+
+            for (int[] query : queries) {
+                int s = query[0];
+                int e = query[1];
+
+                while (s < e) {
+                    char temp = charArray[s];
+                    charArray[s++] = charArray[e];
+                    charArray[e--] = temp;
+                }
+            }
+
+            return new String(charArray);
+        }
+    }
+}
