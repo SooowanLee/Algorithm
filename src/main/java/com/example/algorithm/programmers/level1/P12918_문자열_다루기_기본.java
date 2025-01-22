7b39c8196d6db87c896540db0b6babafef2a446c
Soowan Lee <lsy9406@gmail.com>
2025-01-22 16:16:59 +0900

    feat 정규표현식과, Character.isDigit()을 사용한 풀이

---
+++ src/main/java/com/example/algorithm/programmers/level1/P12918_문자열_다루기_기본.java
@@ -0,0 +1,34 @@
+package com.example.algorithm.programmers.level1;
+
+public class P12918_문자열_다루기_기본 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution("a234"));
+        System.out.println(Solution.solution("1234"));
+    }
+
+    class Solution {
+        public static boolean solution(String s) {
+            boolean answer = true;
+
+            //정규 표현식
+//            if (s.length() != 4 && s.length() != 6) {
+//                return false;
+//            }
+//
+//            return s.matches("[0-9]+");
+
+            //Character.isDigit()
+            if (s.length() != 4 && s.length() != 6) {
+                return false;
+            }
+
+            for (int i = 0; i < s.length(); i++) {
+                if (!Character.isDigit(s.charAt(i))) {
+                    return false;
+                }
+            }
+
+            return answer;
+        }
+    }
+}
