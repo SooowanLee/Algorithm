39b2e3d646d6800f25e83001d62a350a420793f3
Soowan Lee <lsy9406@gmail.com>
2025-01-22 16:39:52 +0900

    feat 이중for문을 사용한 풀이

---
+++ src/main/java/com/example/algorithm/programmers/level1/P12950_행렬의_덧셈.java
@@ -0,0 +1,25 @@
+package com.example.algorithm.programmers.level1;
+
+import java.util.Arrays;
+
+public class P12950_행렬의_덧셈 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(Solution.solution(new int[][]{{1, 2}, {2, 3}}, new int[][]{{3, 4}, {5, 6}})));
+        System.out.println(Arrays.deepToString(Solution.solution(new int[][]{{1}, {2}}, new int[][]{{3}, {4}})));
+    }
+
+    class Solution {
+        public static int[][] solution(int[][] arr1, int[][] arr2) {
+            int[][] answer = {};
+            answer = arr1;
+
+            for (int i = 0; i < arr1.length; i++) {
+                for (int j = 0; j < arr1[i].length; j++) {
+                    arr1[i][j] = arr1[i][j] + arr2[i][j];
+                }
+            }
+
+            return answer;
+        }
+    }
+}
