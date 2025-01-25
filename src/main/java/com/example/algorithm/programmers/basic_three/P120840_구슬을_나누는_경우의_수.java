5c29fc7621d025eb22e9280a0316a1f909cd9923
Soowan Lee <lsy9406@gmail.com>
2025-01-25 23:29:38 +0900

    feat 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/basic_three/P120840_구슬을_나누는_경우의_수.java
@@ -0,0 +1,29 @@
+package com.example.algorithm.programmers.basic_three;
+
+public class P120840_구슬을_나누는_경우의_수 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(3, 2));
+        System.out.println(Solution.solution(5, 3));
+    }
+
+    class Solution {
+        public static int solution(int balls, int share) {
+            if (share == 0 || balls == share) {
+                return 1;
+            }
+
+            return combination(balls, share);
+        }
+
+        private static int combination(int n, int r) {
+            r = Math.min(r, n - r);
+            long result = 1;
+            for (int i = 0; i < r; i++) {
+                result *= (n - i);
+                result /= (i + 1);
+            }
+
+            return (int) result;
+        }
+    }
+}
---
+++ src/main/java/com/example/algorithm/programmers/basic_three/P181830_정사각형으로_만들기.java
@@ -0,0 +1,19 @@
+package com.example.algorithm.programmers.basic_three;
+
+public class P181830_정사각형으로_만들기 {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(new int[][]{{572, 22, 37}, {287, 726, 384}, {85, 137, 292}, {487, 13, 876}}));
+        System.out.println(Solution.solution(new int[][]{{57, 192, 534, 2}, {9, 345, 192, 999}}));
+        System.out.println(Solution.solution(new int[][]{{1, 2}, {3, 4}}));
+    }
+
+    class Solution {
+        public static int[][] solution(int[][] arr) {
+            int[][] answer = {};
+
+
+
+            return answer;
+        }
+    }
+}
