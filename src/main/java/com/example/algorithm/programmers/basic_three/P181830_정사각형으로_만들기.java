6dfcbed4dfcf01740587cd22198e501f7da0b76a
Soowan Lee <lsy9406@gmail.com>
2025-01-27 00:31:33 +0900

    feat Math.max()와 System.arraycopy()를 사용한 풀이

--- src/main/java/com/example/algorithm/programmers/basic_three/P181830_정사각형으로_만들기.java
+++ src/main/java/com/example/algorithm/programmers/basic_three/P181830_정사각형으로_만들기.java
@@ -1,17 +1,26 @@
 package com.example.algorithm.programmers.basic_three;
 
+import java.util.Arrays;
+
 public class P181830_정사각형으로_만들기 {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(new int[][]{{572, 22, 37}, {287, 726, 384}, {85, 137, 292}, {487, 13, 876}}));
-        System.out.println(Solution.solution(new int[][]{{57, 192, 534, 2}, {9, 345, 192, 999}}));
-        System.out.println(Solution.solution(new int[][]{{1, 2}, {3, 4}}));
+        System.out.println(Arrays.deepToString(Solution.solution(new int[][]{{572, 22, 37}, {287, 726, 384}, {85, 137, 292}, {487, 13, 876}})));
+        System.out.println(Arrays.deepToString(Solution.solution(new int[][]{{57, 192, 534, 2}, {9, 345, 192, 999}})));
+        System.out.println(Arrays.deepToString(Solution.solution(new int[][]{{1, 2}, {3, 4}})));
     }
 
     class Solution {
         public static int[][] solution(int[][] arr) {
-            int[][] answer = {};
+            int rowCount = arr.length;
+            int columnCount = arr[0].length;
 
+            int newSize = Math.max(rowCount, columnCount);
 
+            int[][] answer = new int[newSize][newSize];
+
+            for (int i = 0; i < rowCount; i++) {
+                System.arraycopy(arr[i], 0, answer[i], 0, columnCount);
+            }
 
             return answer;
         }
