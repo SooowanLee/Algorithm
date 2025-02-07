726e6002433e6108e446719e8ce768ca744d5505
Soowan Lee <lsy9406@gmail.com>
2025-02-07 16:40:07 +0900

    feat 알고리즘 연습

---
+++ src/main/java/com/example/algorithm/programmers/level1/P17681_1차_비밀지도_2018_KAKAO_BLIND_RECRUITMENT.java
@@ -0,0 +1,19 @@
+package com.example.algorithm.programmers.level1;
+
+public class P17681_1차_비밀지도_2018_KAKAO_BLIND_RECRUITMENT {
+    public static void main(String[] args) {
+        System.out.println(Solution.solution(5, new int[]{9, 20, 28, 18, 11}, new int[]{30, 1, 21, 17, 28}));
+        System.out.println(Solution.solution(6, new int[]{46, 33, 33, 22, 31, 50}, new int[]{27, 56, 19, 14, 14, 10}));
+    }
+
+    class Solution {
+        public static String[] solution(int n, int[] arr1, int[] arr2) {
+            String[] answer = {};
+
+
+
+
+            return answer;
+        }
+    }
+}
