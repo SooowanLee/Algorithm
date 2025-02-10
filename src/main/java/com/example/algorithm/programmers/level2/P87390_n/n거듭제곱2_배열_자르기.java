35a7f5f4c6a3b899526e9a6237c1ec92de8de9d5
Soowan Lee <lsy9406@gmail.com>
2025-02-10 23:26:03 +0900

    feat 주석 추가

--- src/main/java/com/example/algorithm/programmers/level2/P87390_n/n거듭제곱2_배열_자르기.java
+++ src/main/java/com/example/algorithm/programmers/level2/P87390_n/n거듭제곱2_배열_자르기.java
@@ -14,9 +14,13 @@ public class n거듭제곱2_배열_자르기 {
         public static int[] solution(int n, long left, long right) {
             List<Integer> result = new ArrayList<>();
 
+            // 1차원 배열에서 필요한 인덱스만큼만 순회
             for (long i = left; i <= right; i++) {
-                int row = (int) (i / n);
-                int col = (int) (i % n);
+                int row = (int) (i / n); // 행을 구함
+                int col = (int) (i % n); // 열을 구함
+
+                // 1차원 배열에서 인덱스를 찾기 위해 행과 열 중 큰 값을 추출
+                // 인덱스를 제로베이스이기 때문에 +1을 해서 값을 저장
                 result.add(Math.max(row, col) + 1);
             }
 
