36bad4395dcb79da4a4b3114a702950c4af32496
Soowan Lee <lsy9406@gmail.com>
2025-02-11 00:26:33 +0900

    feat 주석 추가

--- src/main/java/com/example/algorithm/programmers/level1/P1845_폰켓몬.java
+++ src/main/java/com/example/algorithm/programmers/level1/P1845_폰켓몬.java
@@ -14,10 +14,13 @@ public class P1845_폰켓몬 {
         public static int solution(int[] nums) {
             int answer = 0;
             Set<Integer> numSet = new HashSet<>();
+
+            //Set에 nums를 저장 중복되는 숫자는 한 개만 저장된다.
             for (int num : nums) {
                 numSet.add(num);
             }
 
+            // N/2와 Set의 크기 중 작은 쪽을 반환
             answer = Math.min(numSet.size(), nums.length / 2);
 
             return answer;
