16a5fa9f539a34f02e4293e26ce6f424bc445c64
SooowanLee <87759519+SooowanLee@users.noreply.github.com>
2025-04-21 23:44:07 +0900

    [level 0] Title: 카운트 업, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.js
@@ -0,0 +1,9 @@
+function solution(start_num, end_num) {
+    var answer = [];
+    
+    // start_num 부터 end_num까지 answer에 넣기
+     for (let i = start_num; i <= end_num; i++){
+         answer.push(i);
+     } 
+    return answer;
+}
