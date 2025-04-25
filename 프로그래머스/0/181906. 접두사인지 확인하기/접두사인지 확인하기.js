8341561013db2d73f7278648c1fb0692a34f6be7
SooowanLee <87759519+SooowanLee@users.noreply.github.com>
2025-04-25 23:50:53 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,10 +1,3 @@
 function solution(my_string, is_prefix) {
-    var answer = 0;
-    
-    if(my_string.startsWith(is_prefix)){
-        answer = 1;
-    } else {
-        answer = 0;
-    }
-    return answer;
+    return my_string.startsWith(is_prefix) ? 1 : 0;
 }
