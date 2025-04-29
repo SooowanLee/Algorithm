136d6bd63ae265c9abea252f1b0b22ba379de2c2
SooowanLee <87759519+SooowanLee@users.noreply.github.com>
2025-04-29 23:48:14 +0900

    [level 0] Title: A 강조하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,10 @@
+function solution(myString) {
+    var answer = '';
+    
+    const lowString = myString.toLowerCase();
+    
+   answer = lowString.replaceAll("a", "A");
+    
+     
+    return answer;
+}
