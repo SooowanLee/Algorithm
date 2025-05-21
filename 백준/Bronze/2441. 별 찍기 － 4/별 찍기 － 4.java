7deef1c29c787de77d88eb173eeb1d1df0b77dc5
SooowanLee <87759519+SooowanLee@users.noreply.github.com>
2025-05-21 15:25:09 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 304 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        for (int i = num; i > 0; i--) {
+            for (int j = 0; j < num - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int k = 0; k < i; k++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
