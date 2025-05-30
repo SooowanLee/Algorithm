c5de6c20e249b8c3da86e7224eabd45292bacc97
SooowanLee <lsy9406@gmail.com>
2025-05-30 15:32:40 +0900

    MOD 코드 수정

--- src/main/java/com/example/algorithms/programmers/level2/P12953_N개의_최소공배수.java
+++ src/main/java/com/example/algorithms/programmers/level2/P12953_N개의_최소공배수.java
@@ -21,9 +21,9 @@ public class P12953_N개의_최소공배수 {
         public static int gcd(int a, int b) {
             //b가 0이 될 때까지 a와 b의 값을 갱신
             while (b != 0) {
-                int temp = b; // 나머지 저장
-                b = a % b; // 나머지로 a를 나누기
-                a = temp; // b를 a위치로 이동
+                int temp = b;
+                b = a % b;
+                a = temp;
             }
 
             return a;
